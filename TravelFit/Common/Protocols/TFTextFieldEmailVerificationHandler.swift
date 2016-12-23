de0bb6afc8a4e6df45b803e6ea7325c806dc46d5
Ronald Hernandez <2ronald.hernandez@gmail.com>
2016-12-23 16:33:16 -0500

    TF: Added pragma marks

--- TravelFit/Common/Protocols/TFTextFieldEmailVerificationHandler.swift
+++ TravelFit/Common/Protocols/TFTextFieldEmailVerificationHandler.swift
@@ -29,6 +29,8 @@ final class TFTextFieldEmailVerificationHandler: NSObject, UITextFieldDelegate {
         return true
     }
 
+    //MARK: Private Helpers
+    
     private func verify(_ email: String) -> Bool {
         let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
         return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: email)
