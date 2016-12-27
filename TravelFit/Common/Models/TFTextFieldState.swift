d5838cf7704b3e35c2af7294e94a837729115752
Ronald Hernandez <2ronald.hernandez@gmail.com>
2016-12-26 23:14:41 -0500

    TF: added TFTextFieldState enum

---
+++ TravelFit/Common/Models/TFTextFieldState.swift
@@ -0,0 +1,14 @@
+//
+//  TFTextFieldState.swift
+//  TravelFit
+//
+//  Created by Ronald Hernandez on 12/26/16.
+//  Copyright © 2016 TravelFit. All rights reserved.
+//
+
+public enum TFTextFieldState {
+
+    case valid
+    case invalid
+
+}
